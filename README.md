40652b3cb54eef8a0ef862e52ba41f209356e878
lucasmauricio <lucasmauriciocmartins@gmail.com>
2016-11-14 23:01:05 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# arrakis
+
+Arrakis is a proof-of-concept project to develop a microservice application.
+
+
+
+
+
+http://dune.wikia.com/wiki/Balut
+
+https://en.wikipedia.org/wiki/Dune_(franchise)
+https://en.wikipedia.org/wiki/List_of_Dune_planets
+
+
+Salusa Secundus
+> antiga capital do império
+
+Kaitain
+> capital atual
+
+Tleilax
+> para logs
+
+Hamonthep
+> fornecedor de escravos
+
+git clone https://github.com/lucasmauricio/arrakis.git 
+sh install.sh
+mvn -f api/registrator/pom.xml clean package
+docker-compose build
+docker-compose up -d
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
+printf "\n${BRIGHT}${WHITE}--------------------------------------------------------------\n"
+printf "${GREEN}Setting up the Arrakis' project${NORMAL}\n"
+
+APP_DIR="app"
+API_DIR="api"
+for DIR_NAME in ${APP_DIR} ${API_DIR}
+do
+  if [ -f $DIR_NAME ] || [ -d $DIR_NAME ]; then
+    printf "\n${BRIGHT}${RED}ERROR!!${NORMAL}\n"
+    printf "There's already a file or directory named ${BRIGHT}${YELLOW}%s/%s${NORMAL}!\nAborting.\n" "$(pwd)" "$DIR_NAME" 1>&2
+    printf "\nPlease, try again at an empty directory.\n"
+    exit 1
+  fi
+done
+
+printf "\nCloning the project's sub-repositories\n"
+
+printf "\n1) a fake API Gateway at Service Discovery repository\n"
+git clone https://github.com/lucasmauricio/service-discovery.git $API_DIR/registrator/
+
+printf "\n2) Collection Service at Balut repository\n"
+git clone https://github.com/lucasmauricio/balut.git $API_DIR/collection/
+
+printf "\n3) User Service at Wallach IX repository\n"
+git clone https://github.com/lucasmauricio/wallach-ix.git $API_DIR/user/
+
+printf "\n${WHITE}The project's setup is complete and now you can use Docker to try it out.\n"
+printf "\n${CYAN}Please, feel free to talk to us at Github (https://github.com/lucasmauricio/arrakis/issues) ${NORMAL}\n"
